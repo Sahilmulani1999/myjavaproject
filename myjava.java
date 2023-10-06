fc8048ea36dcc8501c7f1940629fa9cad2877ffc
sahil <sahilmulani1999@gmail.com>
2023-10-06 06:38:59 +0000

    new

--- myjava.java
+++ myjava.java
@@ -2,7 +2,7 @@ class Simple{
 	
     public static void main(String args[]){  
 	
-     System.out.println("Hello Javaiiiillllli");  
+     System.out.println("Hello Javaiiiillllooooooooli");  
 	
     }  
 	
